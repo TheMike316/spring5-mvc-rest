a8288d96df28058c4f297f3ece4d2952628f46cd
mikeh <mike.hornegger@gmail.com>
2018-07-24 05:32:17 +0200

    Implement category controller

--- src/main/kotlin/com/miho/spring5mvcrest/api/v1/controllers/CategoryController.kt
+++ src/main/kotlin/com/miho/spring5mvcrest/api/v1/controllers/CategoryController.kt
@@ -1,10 +1,23 @@
 package com.miho.spring5mvcrest.api.v1.controllers
 
+import com.miho.spring5mvcrest.api.v1.model.CategoryListDTO
 import com.miho.spring5mvcrest.services.CategoryService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/categories/")
+@RequestMapping("/api/v1/categories")
 class CategoryController(private val categoryService: CategoryService) {
+
+    @GetMapping("/")
+    fun getCategories() = CategoryListDTO(categoryService.getAllCategories()).toResponseEntity()
+
+    @GetMapping("/{name}")
+    fun getCategoryByName(@PathVariable name: String) = categoryService.getCategoryByName(name).toResponseEntity()
 }
+
+fun Any.toResponseEntity() = ResponseEntity(this, HttpStatus.OK)
---
+++ src/main/kotlin/com/miho/spring5mvcrest/api/v1/model/CategoryListDTO.kt
@@ -0,0 +1,3 @@
+package com.miho.spring5mvcrest.api.v1.model
+
+data class CategoryListDTO(var categories: List<CategoryDTO>)
