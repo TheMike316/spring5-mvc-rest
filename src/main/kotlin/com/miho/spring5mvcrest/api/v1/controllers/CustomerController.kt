88a2fc4c5500087ba66c07339e0ce91187b2d424
Michael Hornegger <michael.hornegger@karriere.at>
2018-07-28 09:33:49 +0200

    Bootstrap customer data

--- src/main/kotlin/com/miho/spring5mvcrest/api/v1/controllers/CustomerController.kt
+++ src/main/kotlin/com/miho/spring5mvcrest/api/v1/controllers/CustomerController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/customers")
 class CustomerController(private val customerService: CustomerService) {
 
-    @GetMapping("/")
+    @GetMapping("/", "")
     fun getAllCustomers() = customerService.getAllCustomers().toResponseEntity()
 
     @GetMapping("/{id}")
--- src/main/kotlin/com/miho/spring5mvcrest/bootstrap/Bootstrap.kt
+++ src/main/kotlin/com/miho/spring5mvcrest/bootstrap/Bootstrap.kt
@@ -1,17 +1,23 @@
 package com.miho.spring5mvcrest.bootstrap
 
 import com.miho.spring5mvcrest.domain.Category
+import com.miho.spring5mvcrest.domain.Customer
 import com.miho.spring5mvcrest.repository.CategoryRepository
+import com.miho.spring5mvcrest.repository.CustomerRepository
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Component
 
 @Component
-class Bootstrap(private val categoryRepository: CategoryRepository) : InitializingBean {
+class Bootstrap(private val categoryRepository: CategoryRepository, private val customerRepository: CustomerRepository) : InitializingBean {
 
     override fun afterPropertiesSet() {
         val categories = listOf(Category("Fruits"), Category("Dried"), Category("Fresh"),
                 Category("Exotic"), Category("Nuts"))
 
         categoryRepository.saveAll(categories)
+
+        val customers = listOf(Customer("First", "McFirstface"), Customer("Franz", "Stiegleitner"))
+
+        customerRepository.saveAll(customers)
     }
 }
